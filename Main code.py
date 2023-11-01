e1e4d7356004c80dda0338a4e89e3955f07fc3bc
Kate Groppe <kgroppe@gmail.com>
2023-11-01 11:12:29 -0500

    more code added

--- Main code.py
+++ Main code.py
@@ -12,5 +12,18 @@ print("Input full path thame where intended corpus file or files are stored")
 print("Note: You must enter a quoted string e.g. c:\\simpson\\")
 print()
 
-userSpecifiedPath = raw_input("Path: ")
-result = 
+userSpecifiedPath = input("Path: ")
+result = oNLTK.textCorpusInit(userSpecifiedPath)
+
+if result == 'Success':
+    menuSelection = -1
+    while menuSelection != 0:
+        if menuSelection != -1:
+            print()
+            s = input("Press Enter to Continue...")
+        menuSelection = _NLTKQuery.getUserSelection()
+        if menuSelection == 1:
+            oNLTK.printCorpusLength()
+        if menuSelection == 2:
+            oNLTK.printTokensFound()
+        
