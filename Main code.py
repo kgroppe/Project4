24609f2eacae6693e92049ef113b864d47f0f401
Kate Groppe <kgroppe@gmail.com>
2023-11-01 11:06:43 -0500

    more code added

--- Main code.py
+++ Main code.py
@@ -4,3 +4,13 @@ import _NLTKQuery
 print("Welcome to the NLTK Query Experimentation")
 print("Please wait loading NLTK ...")
 
+import _classNLTKQuery
+oNLTK = _classNLTKQuery.classNLTKQuery()
+
+print()
+print("Input full path thame where intended corpus file or files are stored")
+print("Note: You must enter a quoted string e.g. c:\\simpson\\")
+print()
+
+userSpecifiedPath = raw_input("Path: ")
+result = 
