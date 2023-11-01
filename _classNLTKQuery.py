e8ec5c1d2571ab5becbf72872dded3a8247612df
Kate Groppe <kgroppe@gmail.com>
2023-11-01 13:57:42 -0500

    Adding code

--- _classNLTKQuery.py
+++ _classNLTKQuery.py
@@ -49,3 +49,38 @@ class classNLTKQuery:
         #Ask Dr. Perry about this, whether it should be in a print statement or not.
         print(self.TextCorpus.collocations())
 
+    def searchWordOccurance(self):
+        myWord = input("Enter Search Word:")
+        if myWord:
+            wordCount = self.TextCorpus.count(myWord)
+            #Ask if this works as well for the code
+            print(myWord+" occured: " + wordCount + " times")
+        else:
+            print("Word Entry is Invalid")
+
+    def generateConcordance(self):
+        myWord = input("Enter word to Concord: ")
+        if myWord:
+            self.TextCorpus.concordance(myWord)
+        else:
+            print("Word Entry is Invalid")
+
+    def generateSimilarities(self):
+        myWord = input("Enter seed word: ")
+        if myWord:
+            self.TextCorpus.similar(myWord)
+        else:
+            print("Word Entry is Invalid")
+
+    def printWordIndex(self):
+        myWord = input("Find first occurrence of what Word?: ")
+        if myWord:
+            wordIndex = self.TextCorpus.index(myWord)
+            print("First Occurence of:" + myWord + " is at offset: ")
+            print(wordIndex)
+        else:
+            print("Word Entry is Invalid")
+
+    def printVocabulary(self):
+        print("Compiling Vocabulary Frequencies")
+        
